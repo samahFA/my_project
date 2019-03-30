2a7f889c5623c02bf9e9d17daca2e3d439177b61
samahFA <49093016+samahFA@users.noreply.github.com>
2019-03-30 13:50:10 +0300

    اضافة ملف

---
+++ myfirstproject.java
@@ -0,0 +1,15 @@
+public class myproject{
+ String p="my first project";
+	public myproject (){
+		super();
+		x="the first edit ";
+		y="the third edit ";
+        z="the 4 edit";
+		c="the fivth edit ";
+		s="the last edit"
+		ss="the last last edit"
+		}
+	public void printme (){
+		System.out.print("welcom !!!!!");
+	}
+}
